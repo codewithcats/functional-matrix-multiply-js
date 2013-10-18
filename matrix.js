de6f0c7016f8d856077512c6ae4ccf7283795723
Tanawat Tassana <amp.tanawat@gmail.com>
2013-10-19 02:20:51 +0700

    functional

--- matrix.js
+++ matrix.js
@@ -1,17 +1,10 @@
-function multiplyMatrix(m1, m2) {
-    var result,
-        i, j, k,
-        sum;
-
-    for(i = 0; i < m1.length; i++) {
-        result[i] = [];
-        for(j = 0; j < m2[0].length; j++) {
-            sum = 0;
-            for(k = 0; k < m1[0].length; k++) {
-                sum += m1[i][k] * m2[k][j];
-            }
-            result[i][j] = sum;
-        }
-    }
-    return result;
+function multiplyMatrices(first, second) {
+    var secondColumns = transpose(second);
+    return first.map(function(row){
+        return secondColumns.map(function(column){
+            return column.reduce(function(sum, value, index){
+                return sum + value * row[index];
+            }, 0);
+        });
+    });
 }
